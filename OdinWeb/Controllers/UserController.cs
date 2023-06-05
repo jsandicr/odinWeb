de34b79db4afb01188b0af2228c873199bbf52be
Alfonso Calderon <acalderon30882@ufide.ac.cr>
2023-06-04 21:13:37 -0600

    asd

--- OdinWeb/Controllers/UserController.cs
+++ OdinWeb/Controllers/UserController.cs
@@ -9,16 +9,16 @@ namespace OdinWeb.Controllers
             return View();
         }
 
-        [HttpGet]
-        public ActionResult UsuariosConsulta()
-        {
-            var resultado = modelUsuario.Consultar_Usuarios_Estado(-1);
+        //[HttpGet]
+        //public ActionResult UsuariosConsulta()
+        //{
+        //    var resultado = modelUsuario.Consultar_Usuarios_Estado(-1);
 
-            if (resultado != null && resultado.Codigo == 1)
-                return View(resultado);
-            else
-                return View("Error");
-        }
+        //    if (resultado != null && resultado.Codigo == 1)
+        //        return View(resultado);
+        //    else
+        //        return View("Error");
+        //}
 
     }
 }
