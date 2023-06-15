7151ac2c60f729b747a5f463c28b904736948ca9
KENNETH ARROYO <karroyo80069@ufide.ac.cr>
2023-06-15 02:24:54 +0000

    Model Servicio

---
+++ OdinWeb/Models/Data/Classes/ServicioModel.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using OdinWeb.Models.Data.Interfaces;
+using OdinWeb.Models.Obj;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace OdinWeb.Models.Data.Classes
+{
+    public class ServicioModel : IServicioModel
+    {
+        private readonly IConfiguration _config;
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ServicioModel(IConfiguration config, IHttpContextAccessor httpContextAccessor) {
+
+            _httpContextAccessor = httpContextAccessor;
+            _config = config;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(_config["ApiSettings:BaseUrl"]); // URL base del API
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public Service GetServicioById(int id)
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["Token"];
+            // Agrega el encabezado de autorización con el token
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = _httpClient.GetAsync("api/Service" + id).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var services = response.Content.ReadAsStringAsync().Result;
+                var servicesR = JsonConvert.DeserializeObject<Service>(services);
+                return servicesR;
+            }
+
+            return null;
+        }
+    
+
+        public List<Service> GetServicios()
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["Token"];
+            // Agrega el encabezado de autorización con el token
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = _httpClient.GetAsync("api/Service").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var services = response.Content.ReadAsStringAsync().Result;
+                var servicesR = JsonConvert.DeserializeObject<List<Service>>(services);
+                return servicesR;
+            }
+
+            return null;
+        }
+
+
+        public Service PostServicos(Service service)
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["Token"];
+            // Agrega el encabezado de autorización con el token
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(JsonConvert.SerializeObject(service), Encoding.UTF8, "application/json");
+
+
+            var response = _httpClient.PostAsync("api/Service", content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var services = response.Content.ReadAsStringAsync().Result;
+                var servicesR = JsonConvert.DeserializeObject<Service>(services);
+                return servicesR;
+            }
+
+            return null;
+        }
+
+        public Service PutServicioById(Service service)
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["Token"];
+            // Agrega el encabezado de autorización con el token
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(JsonConvert.SerializeObject(service), Encoding.UTF8, "application/json");
+
+
+            var response = _httpClient.PutAsync("api/Service/" + service.id, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var services = response.Content.ReadAsStringAsync().Result;
+                var servicesR = JsonConvert.DeserializeObject<Service>(services);
+                return servicesR;
+            }
+
+            return null;
+
+        }
+    }
+}
---
+++ OdinWeb/Models/Data/Interfaces/IServicioModel.cs
@@ -0,0 +1,18 @@
+﻿using OdinWeb.Models.Obj;
+
+namespace OdinWeb.Models.Data.Interfaces
+{
+    public interface IServicioModel
+    {
+        public List<Service> GetServicios();
+
+        public Service PostServicos(Service service);
+
+        public Service GetServicioById(int id);
+
+        public Service PutServicioById(Service service);
+
+        public bool DeleteServicioById(int id);
+
+    }
+}
