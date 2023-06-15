5cfb7ddab1018d47faa3a8ce671889ed3632733f
jsand <jsandicr16@gmail.com>
2023-06-14 19:16:25 -0600

    CRUD Cliente

--- OdinWeb/Controllers/UserController.cs
+++ OdinWeb/Controllers/UserController.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using OdinWeb.Models.Obj;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace OdinWeb.Controllers
 {
     public class UserController : Controller
     {
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Dashboard()
         {
             return View();
         }
-
     }
 }
