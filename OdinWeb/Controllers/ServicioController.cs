6ddd911508f133faa0177dc86759bec4d66e4756
KennethArroyoVargas <karroyo80069@gmail.com>
2023-06-21 11:24:14 -0600

    Cambios

--- OdinWeb/Controllers/ServicioController.cs
+++ OdinWeb/Controllers/ServicioController.cs
@@ -180,6 +180,7 @@ namespace OdinWeb.Controllers
                     s.name = servico.name;
                     s.description = servico.description;
                     s.active = servico.active;
+                    s.photo = servico.photo;
                     ViewData["Estados"] = estados;
                     return View(s);
 
@@ -214,6 +215,14 @@ namespace OdinWeb.Controllers
                 var extension = Path.GetExtension(nombreArchivo);
                 if (extension == ".png" || extension == ".jpg")
                 {
+                    if (!string.IsNullOrEmpty(s.photo))
+                    {
+                        var rutaImagenAnterior = Path.Combine(hostingEnvironment.WebRootPath, "images", "services", s.photo);
+                        if (System.IO.File.Exists(rutaImagenAnterior))
+                        {
+                            System.IO.File.Delete(rutaImagenAnterior);
+                        }
+                    }
                     var nombreUnico = Guid.NewGuid().ToString() + extension;
 
                     var rutaGuardar = Path.Combine(hostingEnvironment.WebRootPath, "images", "services", nombreUnico);
