b5f5e42caed43d471cfb7287a37e6f5ee6cbd6f9
KennethArroyoVargas <karroyo80069@gmail.com>
2023-07-09 15:33:04 -0600

    Cambios

---
+++ OdinWeb/Models/Obj/Document.cs
@@ -0,0 +1,14 @@
+﻿namespace OdinApi.Models.Obj
+{
+    public class Document
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public int idUser { get; set; }
+        public int idTicket { get; set; }
+
+        public string nameDocument { get; set; }
+        public User? user { get; set; }
+        public Ticket? ticket { get; set; }
+    }
+}
