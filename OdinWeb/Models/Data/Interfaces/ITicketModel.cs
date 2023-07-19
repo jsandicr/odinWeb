602ab531239857f4648c39cc38ca2b371a8b2691
KennethArroyoVargas <karroyo80069@gmail.com>
2023-07-19 17:46:50 -0600

    Cambios

--- OdinWeb/Models/Data/Interfaces/ITicketModel.cs
+++ OdinWeb/Models/Data/Interfaces/ITicketModel.cs
@@ -5,7 +5,7 @@ namespace OdinWeb.Models.Data.Interfaces
     public interface ITicketModel
     {
         public List<Ticket> GetTickets();
-        public List<Ticket> GetAssignedTickets(string id);
+        public List<Ticket> GetAssignedTickets(string status);
         public List<Ticket> GetOpenTickets();
         public Ticket PostTicket(Ticket ticket);
         public Ticket GetTicketById(int id);
