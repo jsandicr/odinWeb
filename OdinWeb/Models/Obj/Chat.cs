78465ca91806d0eee3f366b95ce3fee0ecf8c547
Xerarth19 <101201557+Xerarth19@users.noreply.github.com>
2023-08-08 23:02:51 -0600

    Chat

---
+++ OdinWeb/Models/Obj/Chat.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdinWeb.Models.Obj
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Answer { get; set; }
+    }
+}
