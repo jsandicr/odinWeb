fcd148e60ce7950024d441c90a07d59f4528b940
KennethArroyoVargas <karroyo80069@gmail.com>
2023-08-09 00:04:34 -0600

    Cambios

---
+++ OdinWeb/Controllers/ErrorLogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdinWeb.Models.Data.Interfaces;
+
+namespace OdinWeb.Controllers
+{
+    public class ErrorLogController : Controller
+    {
+        private readonly ITransLogModel _transLogModel;
+
+        public ErrorLogController(ITransLogModel transLogModel)
+        {
+            _transLogModel = transLogModel;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            var error = await _transLogModel.GetAsyncE();
+            return View(error);
+        }
+    }
+}
--- OdinWeb/Models/Obj/ErrorLog.cs
+++ OdinWeb/Models/Obj/ErrorLog.cs
@@ -1,12 +1,24 @@
-﻿namespace OdinWeb.Models.Obj
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace OdinWeb.Models.Obj
 {
     public class ErrorLog
     {
         public int id { get; set; }
+        [Display(Name = "Código")]
         public int code { get; set; }
+
+        [Display(Name = "Descripción")]
         public string description { get; set; }
+
+        [Display(Name = "Fecha")]
         public DateTime date { get; set; }
+
+        [Display(Name = "ID de Usuario")]
         public int idUser { get; set; }
+
+        [Display(Name = "Usuario")]
         public User? user { get; set; }
     }
 }
