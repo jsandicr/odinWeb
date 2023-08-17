467880a0ea1332ffe1f8b86321edf93d1118c2b1
GERALD NARVAEZ <gnarvaez70785@ufide.ac.cr>
2023-08-16 20:00:21 -0600

    cambios en la validacion de preguntas frecuentes

--- OdinWeb/Models/Obj/Chat.cs
+++ OdinWeb/Models/Obj/Chat.cs
@@ -5,7 +5,15 @@ namespace OdinWeb.Models.Obj
     public class Chat
     {
         public int Id { get; set; }
+
+        [Display(Name = "Pregunta")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(300, ErrorMessage = "El campo no puede contener más de 300 caracteres")]
         public string Text { get; set; }
+
+        [Display(Name = "Respuesta")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(1000, ErrorMessage = "El campo no puede contener más de 1000 caracteres")]
         public string Answer { get; set; }
     }
 }
