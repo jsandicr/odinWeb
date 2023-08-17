aaadafa02ba56d0bf2ea97740aa85e8d5c7111e1
KennethArroyoVargas <karroyo80069@gmail.com>
2023-08-16 20:31:55 -0600

    Cambios

--- OdinWeb/Models/Data/Interfaces/IServicioModel.cs
+++ OdinWeb/Models/Data/Interfaces/IServicioModel.cs
@@ -7,7 +7,7 @@ namespace OdinWeb.Models.Data.Interfaces
         public List<Service> GetServicios();
         public List<Service> GetServiciosStatus(bool status);
         public bool PostServicos(Service service);
-        public Service GetServicioById(int id);
+        public Service GetServicioById(int? id);
         public bool PutServicioById(Service service);
         public bool DeleteServicioById(int id);
         public List<Service> GetListSubServicioById(long id);
